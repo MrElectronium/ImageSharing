faaa94727923a920f6477716f7c12f829c9774ff
MrElectronium <46976544+MrElectronium@users.noreply.github.com>
2023-11-12 23:21:39 +0300

    Update mock.js

--- src/mock.js
+++ src/mock.js
@@ -1,2 +1,2 @@
-const arr=[{id:"0",src:"./pics/00100-3543327826.png",content:"Mavimsi bir atmosfer uzak bir gezegen resmi"}];
+const arr=[{id:"0",src:"./pics/00100-3543327826.jpeg",content:"Mavimsi bir atmosfer uzak bir gezegen resmi"}];
 export default arr;
